8f99464a49b6a49593f521e79ecaf2870846b69a
Arturo Verbel <arturo-verbel@hotmail.com>
2018-11-22 06:26:32 -0500

    Add imagen

--- index.py
+++ index.py
@@ -4,18 +4,25 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-data = pd.read_csv('orders_users.csv')
-lat = data['lat']
-lng = data['lng']
+data = pd.read_csv('orders_users_created_on_enero_2018_11_20.csv')
+time_order = data['created_at']
+price_order = data['total_price']
+
+time_order = [element[11:-6] for element in time_order]
+
+short_data_1 = []
+short_data_2 = []
 
 count = 0
-for index, val in enumerate(lat):
-    if count == 100:
+for index, val in enumerate(time_order):
+    if count == 300:
         break
-    print(lat[index], lng[index])
+    print(time_order[index], price_order[index])
+    short_data_1.append([time_order[index]])
+    short_data_2.append([price_order[index]])
     count += 1
 
 plt.figure(figsize=(8, 6))
-plt.scatter(lat, lng, marker='_', color='red')
+plt.scatter(short_data_1, short_data_2, marker='_', color='red')
 plt.show()
 
