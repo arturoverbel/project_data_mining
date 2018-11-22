71fd4b5cc87597694c961293fe3e7f3fb767f11f
Arturo Verbel <arturo-verbel@hotmail.com>
2018-11-22 06:43:48 -0500

    Reroa

--- index.py
+++ index.py
@@ -5,10 +5,16 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 
 data = pd.read_csv('orders_users_created_on_enero_2018_11_20.csv')
-time_order = data['created_at']
-price_order = data['total_price']
 
+time_order = data['created_at']
 time_order = [element[11:-6] for element in time_order]
+
+price_order = data['total_price']
+tip_by_order = data['tip']
+
+
+data_1 = time_order
+data_2 = price_order
 
 short_data_1 = []
 short_data_2 = []
@@ -17,9 +23,9 @@ count = 0
 for index, val in enumerate(time_order):
     if count == 300:
         break
-    print(time_order[index], price_order[index])
-    short_data_1.append([time_order[index]])
-    short_data_2.append([price_order[index]])
+    print(data_1[index], data_2[index])
+    short_data_1.append([data_1[index]])
+    short_data_2.append([data_2[index]])
     count += 1
 
 plt.figure(figsize=(8, 6))
